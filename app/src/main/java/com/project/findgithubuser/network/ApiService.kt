7984272c81d3e909efcec67b9f1098592bfe8347
Frengky <fsinaga9c@gmail.com>
2020-09-18 07:40:59 +0700

    Dicoding Submission

--- app/src/main/java/com/project/findgithubuser/network/ApiService.kt
+++ app/src/main/java/com/project/findgithubuser/network/ApiService.kt
@@ -1,29 +1,23 @@
 package com.project.findgithubuser.network
 
-import com.project.findgithubuser.BuildConfig
 import com.project.findgithubuser.models.DataItem
 import com.project.findgithubuser.models.DetailUser
 import com.project.findgithubuser.models.ResponseUser
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
     @GET("/search/users")
     fun getListUsers(@Query("q") q: String): Call<ResponseUser>
 
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
     @GET("/users/{username}")
     fun getDetailUser(@Path("username") user: String): Call<DetailUser>
 
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
     @GET("/users/{username}/followers")
     fun getFollowersUser(@Path("username") user: String): Call<List<DataItem>>
 
-    @Headers("Authorization: token ${BuildConfig.ApiKey}")
     @GET("/users/{username}/following")
     fun getFollowingUser(@Path("username") user: String): Call<List<DataItem>>
 }
